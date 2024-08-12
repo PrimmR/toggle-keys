57aaa715aa1725e2c3c51e6d6159a7efb1e41496
PrimmR <primmr@icloud.com>
2024-08-12 12:43:30 +0100

    Add num-lock, scroll-lock & kana-lock for linux

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ This is a WIP crate that provides an interface for querying the state of togglab
 
 This crate is very much still in development, so it currently only has limited functionality. Currently:
 
-- Only the `Caps Lock` key can be queried
+- The `Caps Lock`, `Num Lock`, `Scroll Lock` & `Kana Lock` keys can be queried
 - Windows has full support
 - Linux has partial support, as the user needs to have input privileges to fetch the state of the keyboard
 - macOS has no support
 
-I intend to flesh out this library somewhat by adding a wider range of keys, but I'm unlikely to improve the OS support myself.
+I'm unlikely to improve the OS support myself, but any form of contribution is welcome!
--- src/linux.rs
+++ src/linux.rs
@@ -1,28 +1,31 @@
 use evdev::Key as KeyCode;
 use evdev::LedType as LedCode;
 
-pub fn get_caps_lock_state() -> bool {
+fn get_toggle_state(key: KeyCode, led: LedCode) -> bool {
     let devices: Vec<_> = evdev::enumerate()
         .filter(|x| {
             x.1.supported_keys()
-                .map_or(false, |keys| keys.contains(KeyCode::KEY_CAPSLOCK))
+                .map_or(false, |keys| keys.contains(key))
         })
         .collect();
 
-    devices.iter().any(|d| {
-        d.1.get_led_state()
-            .map_or(false, |s| s.contains(LedCode::LED_CAPSL))
-    })
+    devices
+        .iter()
+        .any(|d| d.1.get_led_state().map_or(false, |s| s.contains(led)))
+}
+
+pub fn get_caps_lock_state() -> bool {
+    get_toggle_state(KeyCode::KEY_CAPSLOCK, LedCode::LED_CAPSL)
 }
 
 pub fn get_num_lock_state() -> bool {
-    unimplemented!()
+    get_toggle_state(KeyCode::KEY_NUMLOCK, LedCode::LED_NUML)
 }
 
 pub fn get_scroll_lock_state() -> bool {
-    unimplemented!()
+    get_toggle_state(KeyCode::KEY_SCROLLLOCK, LedCode::LED_SCROLLL)
 }
 
 pub fn get_kana_lock_state() -> bool {
-    unimplemented!()
+    get_toggle_state(KeyCode::KEY_KATAKANA, LedCode::LED_KANA)
 }
